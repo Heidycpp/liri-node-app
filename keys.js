139b1cdaf40952f49ef6468bab5914207f212ab1
Heidycpp <31572400+Heidycpp@users.noreply.github.com>
2017-11-15 23:15:04 -0500

    heidy

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'fjMFXGqyNKXYjlDM6dKvAWp1o',
+  consumer_secret: '8rWEDDOBX4eyh31ZIvcvREOfNaupqx0TRKCJml4e0BYJ2iKZ2t',
+  access_token_key: '930599426019807238-n360kKlalc1CFJO9UUm24RAV7PCQ1BV',
+  access_token_secret: 'HXrkpeyYJcUo2J20h75HWTOHJ8nBCGScwTjkIL2CiWIBG',
+};
